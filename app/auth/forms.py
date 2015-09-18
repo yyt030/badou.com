ec87ef801d99868bb441fe7c1e37fd05e2d2f8a4
yueyt <yyt030@gmail.com>
2015-09-18 10:10:16 +0800

    update

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
@@ -6,11 +7,11 @@ from ..models import User
 
 
 class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField(u'邮箱', validators=[Required(), Length(1, 64),
                                              Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    password = PasswordField(u'密码', validators=[Required()])
+    remember_me = BooleanField(u'记住我')
+    submit = SubmitField(u'登录')
 
 
 class RegistrationForm(Form):
